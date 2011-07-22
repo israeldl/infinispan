1e1d1ff12783a05b9d47337ca7c1263c61c474bf
Israel <israeldl@gmail.com>
2011-07-22 09:15:33 -0300

    ISPN-200 - Fixs for the compile errors (new version of Hibernate Search

--- query/src/main/java/org/infinispan/query/backend/SearchableCacheConfiguration.java
+++ query/src/main/java/org/infinispan/query/backend/SearchableCacheConfiguration.java
@@ -91,4 +91,10 @@ public class SearchableCacheConfiguration implements SearchConfiguration {
    public Map<Class<? extends ServiceProvider<?>>, Object> getProvidedServices() {
       return Collections.emptyMap();
    }
+
+   @Override
+   public boolean isTransactionManagerExpected() {
+	  // TODO Auto-generated method stub
+      return false;
+   }
 }
